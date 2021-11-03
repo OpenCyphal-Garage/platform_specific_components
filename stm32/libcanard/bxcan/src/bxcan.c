a3d9102163fd473731be3c1fe94be7d35cd0b596
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-11-03 11:17:15 +0200

    Fix linter warning

--- stm32/libcanard/bxcan/src/bxcan.c
+++ stm32/libcanard/bxcan/src/bxcan.c
@@ -43,14 +43,14 @@ extern uint32_t SystemCoreClock;
 
 /// Error trackers, one for each possible CAN interface.
 /// As the bxCANReapError() function only returns a bool, we can keep this as a simple flag.
-static bool g_error[2] = {false};
+static bool g_error[2] = {false};  // NOLINT mutable global
 
 /// Time stamps for TX time-out management. There are three TX mailboxes for each CAN interface.
 /// An UINT64_MAX value means the time stamp is not set (mailbox is not in use). Zero initialization
 /// will work, as the value will be re-set to UINT64_MAX in abortExpiredTxMailboxes() and set to the
 /// required value in bxCANPush(). This way, any inadvertently busy mailboxes will still be aborted
 /// automatically, making this more robust than initializing to UINT64_MAX from the onset.
-static uint64_t g_tx_deadline[(1 + BXCAN_MAX_IFACE_INDEX) * 3] = {0};
+static uint64_t g_tx_deadline[(1 + BXCAN_MAX_IFACE_INDEX) * 3] = {0};  // NOLINT mutable global
 
 /// Converts an extended-ID frame format into the bxCAN TX ID register format.
 static uint32_t convertFrameIDToRegister(const uint32_t id)
